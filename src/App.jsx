4f42d5e3168cb035b3eb7b99da36d1d214abecde
infoflorith-dev <infoflorith@gmail.com>
2026-06-11 11:46:00 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 function App() {
   return (
     <div className="app">
-      <h1>🚜 WPK Weekprestatie</h1>
+      <div className="poster">
 
-      <div className="hero">
-        <h2>Week 23</h2>
-        <div className="score">+412 uur</div>
-        <p>Beter gepresteerd dan de norm</p>
+        <h1 className="title">WEEKPRESTATIE</h1>
+
+        <h2 className="week">WEEK 23</h2>
+
+        <div className="buttons">
+          <button>📊 Excel kiezen</button>
+          <button>📄 PDF downloaden</button>
+        </div>
+
+        <div className="hero">
+          <div className="worker">👨‍🌾</div>
+
+          <div className="hero-center">
+            <p>TEAM WPK HEEFT DEZE WEEK</p>
+
+            <div className="score">
+              +412 UUR
+            </div>
+
+            <p>BETER GEPRESTEERD DAN DE NORM</p>
+          </div>
+
+          <div className="worker">👩‍🌾</div>
+        </div>
+
       </div>
     </div>
   );
